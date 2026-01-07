df84e254ad690c6fb6a2b470b97a6bc920ec9f78
Andre Cauê <andrecaue@outlook.com>
2026-01-07 01:09:01 -0300

    removing create table script

--- Backend/create_tables.py
+++
@@ -1,8 +0,0 @@
-from app.database import engine, Base
-from app.models import *  # importe todos os modelos aqui (ou ajuste o caminho)
-
-if __name__ == "__main__":
-    print("Criando todas as tabelas...")
-    Base.metadata.drop_all(bind=engine)  # opcional: limpa tudo primeiro
-    Base.metadata.create_all(bind=engine)
-    print("Tabelas criadas com sucesso!")
