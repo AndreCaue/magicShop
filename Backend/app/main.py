0f7bd11a84de09868c0dff06b5b7658f3e956936
Andre Cauê <andrecaue@outlook.com>
2026-02-09 22:47:26 -0300

    re-deploy

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -21,7 +21,8 @@ from .core.config import settings
 if settings.ENVIRONMENT == "development":
     models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(docs_url="/docs", title=f"{settings.APP_NAME}".  redirect_slashes=False)
+app = FastAPI(docs_url="/docs",
+              title=f"{settings.APP_NAME}",  redirect_slashes=False)
 
 
 @app.get("/docs", include_in_schema=False)
